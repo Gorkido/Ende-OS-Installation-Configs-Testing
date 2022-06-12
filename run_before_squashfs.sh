3f13da4d27e28683d4754a0eb6659f1ed732bec8
gorkido <gorkido@owner>
2022-06-12 19:43:25 +0300

    lightdm

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -97,7 +97,8 @@ ln -s "/usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png" "/usr/share
 # sudo sed -i 's/^webkit_theme        = .*$/webkit_theme        = litarvan/' /etc/lightdm/lightdm-webkit2-greeter.conf
 
 # Fix LightDM
-sudo pacman -Sy --noconfirm lightdm-slick-greeter
+pacman -Sy --noconfirm lightdm-slick-greeter
+systemctl enable lightdm.service
 
 # TEMPORARY CUSTOM FIXES
 
